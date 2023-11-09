53eea08ea07e8b88816bdbb19646fdcc14b10960
JaviGalFer <fgalfer299@g.educaand.es>
2023-11-09 20:09:26 +0100

    update

--- JavaScript/11 - Condicionales/repaso-form/js/script.js
+++ JavaScript/11 - Condicionales/repaso-form/js/script.js
@@ -1,29 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
     const puestoTrabajoInput = document.getElementById("puestoTrabajo");
     const salarioResultado = document.getElementById("salarioResultado");
+    const noInfoMessage = document.getElementById("noInfoMessage");
     const btnSalario = document.getElementById("btnSalario");
+    const resultContainer = document.getElementById("resultContainer");
 
     btnSalario.addEventListener("click", () => {
         consultarSalario();
     });
 
     const baseDatosSalarios = [
-        { puesto: 'dba job', salario: 70000, detalle: 'Administrador de bases de datos' },
-        { puesto: 'front-end developer', salario: 45000, detalle: 'Desarrollador de Front-end' },
-        { puesto: 'back-end developer', salario: 50000, detalle: 'Desarrollador de Back-end' }
+        { puesto: 'project manager', salario: 80000, detalle: 'Gerente de Proyecto' },
+        { puesto: 'data analyst', salario: 60000, detalle: 'Analista de Datos' },
+        { puesto: 'software engineer', salario: 65000, detalle: 'Ingeniero de Software' }
     ];
 
     function consultarSalario() {
         const puestoTrabajo = puestoTrabajoInput.value;
-        let salarioInfo = "Puesto no encontrado";
+        let salarioInfo = "";
+        let displayStyle = "block";
 
         const salarioEncontrado = baseDatosSalarios.find(item => item.puesto === puestoTrabajo);
 
         if (salarioEncontrado) {
             salarioInfo = `Puesto: ${salarioEncontrado.detalle}, Salario aproximado: ${salarioEncontrado.salario} euros`;
+            salarioResultado.style.display = "block";
+            noInfoMessage.style.display = "none";
+        } else {
+            salarioInfo = "";
+            displayStyle = "none";
+            salarioResultado.style.display = "none";
+            noInfoMessage.style.display = "block";
         }
 
+        resultContainer.style.display = displayStyle;
         salarioResultado.textContent = salarioInfo;
     }
 });
-    
